beb497fc2cb48a509c9cfc4fdcd3b7176a54c712
Park chunsung <popobaboya@gmail.com>
2020-04-11 20:36:59 +0900

    Add fileIO method in manager.h

--- manager.h
+++ manager.h
@@ -1,2 +1,4 @@
     void loopRead(Product *s[], int count); //readProduct함수 반복(제품 전체 출력)
     int selectDataNo(Product *s[], int count); //데이터 수정, 삭제 시 번호 선택 함수
+    void saveData(Product *s[], int count); 
+    int loadData(Product *s[]);
